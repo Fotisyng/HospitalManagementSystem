ac499031c2da736ae88d73bb4ec606cc6afda6ec
Fotisyng <128265867+Fotisyng@users.noreply.github.com>
2024-10-05 14:06:56 +0300

    Nurse functionality (#2)
    * feat: Create the django project
    
    * feat: connect to the hospital db
    
    * feat: First migration on patients, emergency_contacts and addresses
    
    * feat: Add insurance and prescription models
    
    * feat: Add the doctors model
    
    * feat: Create doctor form
    
    * feat: Add list doctors view and have better rendering
    
    * feat: Add update and delete doctor views
    
    * feat: Add address validation
    
    * feat: Better visualization of pages
    
    * fix: Add sensitive variables in a .env file
    
    * feat: make configurable the address validation and provide requirements & mock .env
    
    * feat: Add the patient creation form
    
    * feat: List, Update, Delete Patients
    
    * feat: basic nurse functionality
    
    * feat: Remaining Nurse functionality added

--- src/hospitalManagementSystem/urls.py
+++ src/hospitalManagementSystem/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('doctors/', include('doctors.urls')),
-    path('patients/', include('patients.urls'))
+    path('patients/', include('patients.urls')),
+    path('nurses/', include('nurses.urls'))
 ]
